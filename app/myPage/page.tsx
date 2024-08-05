d70cad925e2a40bebebeb2f22ee4b8bf62a8038e
kim gayoung <kky36907960@gmail.com>
2024-08-05 16:31:22 +0900

    mypage update

--- app/myPage/page.tsx
+++ app/myPage/page.tsx
@@ -4,7 +4,7 @@ const myPage = () => {
       <div>
         <p>회원정보 확인/수정</p>
         <p>보람</p>
-        이름 : <input type="text" autofocus placeholder="아이디" size="10" maxlength="4" /><br />
+        이름 : <input type="text" autofocus placeholder="아이디" size={10} maxlength="4" /><br />
         아이디 : <input type="text" autofocus placeholder="아이디" size="10" maxlength="4" /><br />
         비밀번호 : <input type="password" /><br />
         비밀번호 확인 : <input type="password" /><br />
