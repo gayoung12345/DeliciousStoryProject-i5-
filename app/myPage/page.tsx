f8a63a7505672bea802d5b0c936e23b8c8df5986
kim gayoung <kky36907960@gmail.com>
2024-08-05 16:32:57 +0900

    boram - mypage

--- app/myPage/page.tsx
+++ app/myPage/page.tsx
@@ -4,11 +4,30 @@ const myPage = () => {
       <div>
         <p>회원정보 확인/수정</p>
         <p>보람</p>
-        이름 : <input type="text" autofocus placeholder="아이디" size={10} maxlength="4" /><br />
-        아이디 : <input type="text" autofocus placeholder="아이디" size="10" maxlength="4" /><br />
-        비밀번호 : <input type="password" /><br />
-        비밀번호 확인 : <input type="password" /><br />
-        이메일 : <input type="email" value="test@naver.com" required /><br />
+        이름 :{" "}
+        <input
+          type="text"
+          autoFocus
+          placeholder="아이디"
+          size={10}
+          maxLength={4}
+        />
+        <br />
+        아이디 :{" "}
+        <input
+          type="text"
+          autoFocus
+          placeholder="아이디"
+          size={10}
+          maxLength={4}
+        />
+        <br />
+        비밀번호 : <input type="password" />
+        <br />
+        비밀번호 확인 : <input type="password" />
+        <br />
+        이메일 : <input type="email" value="test@naver.com" required />
+        <br />
         <input type="submit" value="수정하기" />
       </div>
     </main>
