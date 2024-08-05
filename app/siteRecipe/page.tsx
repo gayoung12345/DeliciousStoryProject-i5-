053726b4c3becd0d9a9b29d1be86ffdcc986d2de
lde <lde000325@naver.com>
2024-08-05 16:16:44 +0900

    다은 레시피페이지

--- app/siteRecipe/page.tsx
+++ app/siteRecipe/page.tsx
@@ -1,10 +1,82 @@
+import Image from "next/image";
+
 const siteRecipe = () => {
   return (
     <main>
+      
+      <header>헤더</header>
+
       <div>
-        <p>공식레시피 페이지</p>
-        <p>다은</p>
+        <h1>레시피 페이지</h1>
+
+        <table>
+          <tr>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+          </tr>
+          <tr>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+          </tr>
+          <tr>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+            <a href="#"><td>
+              <Image src="/svg/next.svg" />
+              <div><p>레시피 이름</p></div>
+            </td></a>
+          </tr>
+        </table>
       </div>
+
+      <div> 페이지 숫자 1/2/3/4/5 ... </div>
+
+      <div>
+        <input type="search" /> <button type="submit" value="검색"></button>
+      </div>
+
+
+      <footer>푸터</footer>
+
+
     </main>
   );
 };
