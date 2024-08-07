5817316b9da9ffe569911c837b54ac49de8454f9
boram <boo6332@gmail.com>
2024-08-07 16:40:03 +0900

    마이페이지 수정

--- app/myPage/page.tsx
+++ app/myPage/page.tsx
@@ -1,10 +1,15 @@
+import { StyleSheet, Text } from "react-native";
+
 const myPage = () => {
   return (
     <main>
-      <div>
-        <p>회원정보 확인/수정</p>
-        <p>보람</p>
-        이름 :{" "}
+      <div style={styles.head}>
+        <p>회원정보 확인/수정하기</p>
+      </div>
+
+      <div style={styles.container}>
+        <div>
+          이름 :{" "}
         <input
           type="text"
           autoFocus
@@ -12,8 +17,9 @@ const myPage = () => {
           size={10}
           maxLength={4}
         />
-        <br />
-        아이디 :{" "}
+        </div>
+        <div>
+          아이디 :{" "}
         <input
           type="text"
           autoFocus
@@ -21,17 +27,39 @@ const myPage = () => {
           size={10}
           maxLength={4}
         />
-        <br />
-        비밀번호 : <input type="password" />
-        <br />
-        비밀번호 확인 : <input type="password" />
-        <br />
-        이메일 : <input type="email" value="test@naver.com" required />
-        <br />
-        <input type="submit" value="수정하기" />
+        </div>
+        <div>
+          비밀번호 : <input type="password" />
+        </div>
+        <div>
+          비밀번호 확인 : <input type="password" />
+        </div>
+        <div>
+          이메일 : <input type="email" value="test@naver.com" required />
+        </div>
+        <div>
+          <input type="submit" value="수정하기" />
+        </div>
       </div>
     </main>
   );
 };
+
+const styles = StyleSheet.create({
+  head: {
+    height: 200,
+    marginTop: 100,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 32,
+  },
+  container: {
+    width: 500,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#383838',
+  },
+});
 
 export default myPage;
