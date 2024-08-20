c94e47bfb39287962b5bf37c93bf220f30ec1f30
kim gayoung <kky36907960@gmail.com>
2024-08-20 12:17:38 +0900

    에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,8 @@ const nextConfig = {
             '2bob.co.kr',
         ], // 허용할 도메인 추가
     },
-    output: 'export', // 정적 HTML 파일 생성을 위한 설정
-    distDir: 'out', // 빌드 아티팩트를 'out' 폴더에 저장
+    // output: 'export', // 정적 HTML 파일 생성을 위한 설정
+    // distDir: 'out', // 빌드 아티팩트를 'out' 폴더에 저장
 };
 
 export default withGluestackUI(nextConfig);
